fd921c0d6a6b5e7638e103c4b9a885f8846b9283
Drak <drak@zikula.org>
2012-12-23 13:02:04 +0000

    Relocate files

--- Version.php
+++ Version.php
@@ -30,8 +30,8 @@ class Profile_Version extends Zikula_AbstractVersion
 
             'url'           => $this->__('profile'),
 
-            'version'       => '1.6.1',
-            'core_min' => '1.3.0', // Fixed to 1.3.x range
+            'version'       => '1.6.2',
+            'core_min' => '1.3.6-dev', // Fixed to 1.3.x range
             'core_max' => '1.3.99', // Fixed to 1.3.x range
 
             'capabilities'  => array(
