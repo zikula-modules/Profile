dc18351e088fb94822f85cd0134791662d516596
Shefik <shefik@allknightaccess.com>
2013-12-07 17:32:05 -0500

    Correcting missing default icon.

--- Zikula/Module/ProfileModule/Resources/public/js/showavatar.js
+++ Zikula/Module/ProfileModule/Resources/public/js/showavatar.js
@@ -3,6 +3,10 @@
 function showavatar()
 {
     if ($('prop_avatar') && $('youravatardisplay')) {
-        $('youravatardisplay').src = Zikula.Config.baseURL + $('youravatarpath').innerHTML + '/' + $('prop_avatar').options[$('prop_avatar').selectedIndex].value;
+        if ($('prop_avatar').options[$('prop_avatar').selectedIndex].value == '') {
+            $('youravatardisplay').src = '//www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.jpg?d=mm&r=g&s=80&f=1';
+        } else {
+            $('youravatardisplay').src = Zikula.Config.baseURL + $('youravatarpath').innerHTML + '/' + $('prop_avatar').options[$('prop_avatar').selectedIndex].value;
+        }
     }
 }
