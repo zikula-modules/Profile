e102c80b69b7cb4643d8b9314c982217680db7b0
Craig Heydenburg <craigh@mac.com>
2014-01-10 15:46:46 -0500

    correct copy/paste error on file header

--- Zikula/Module/ProfileModule/DependencyInjection/ZikulaProfileExtension.php
+++ Zikula/Module/ProfileModule/DependencyInjection/ZikulaProfileExtension.php
@@ -1,12 +1,16 @@
 <?php
 
 /**
- * Dizkus
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
  *
- * @copyright (c) 2001-now, Dizkus Development Team
- * @link https://github.com/zikula-modules/Dizkus
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package Dizkus
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Profile
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
  */
 
 namespace Zikula\Module\ProfileModule\DependencyInjection;
