ee10018cc805ab0301a5693d84889417847433fd
Shefik <shefik@allknightaccess.com>
2014-06-25 01:45:28 -0400

    Removing redundant word.

--- Zikula/Module/ProfileModule/Api/AccountApi.php
+++ Zikula/Module/ProfileModule/Api/AccountApi.php
@@ -53,7 +53,7 @@ class AccountApi extends \Zikula_AbstractApi
             $items[] = array(
                 'url' => ModUtil::url($this->name, 'user', 'view', array('uid' => $uid)),
                 'module' => $this->name,
-                'title' => $this->__('Your Profile'),
+                'title' => $this->__('Profile'),
                 'icon' => 'admin.png');
             if (SecurityUtil::checkPermission($this->name.':Members:', '::', ACCESS_READ)) {
                 $items[] = array(
