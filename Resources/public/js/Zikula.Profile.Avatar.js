d872fe1b69bef822f64114a0b5ffe2255f46d141
Guite <info@guite.de>
2016-12-02 13:00:21 +0100

    added missing closing bracket, fixed #62

--- Resources/public/js/Zikula.Profile.Avatar.js
+++ Resources/public/js/Zikula.Profile.Avatar.js
@@ -7,7 +7,7 @@
             if ($('#prop_avatar').value() == '') {
                 $('#avatarPreview').attr('src' ,'//www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.jpg?d=mm&r=g&s=80&f=1');
             } else {
-                $('#avatarPreview').attr('src', Zikula.Config.baseURL + $('#avatarPath').html() + '/' + $('#prop_avatar').val();
+                $('#avatarPreview').attr('src', Zikula.Config.baseURL + $('#avatarPath').html() + '/' + $('#prop_avatar').val());
             }
         }
     }
