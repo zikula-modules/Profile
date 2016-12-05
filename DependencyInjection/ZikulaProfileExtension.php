2a7d8f1f0d2d02b335aa3b3d827a2c45f0cdcf4d
Guite <info@guite.de>
2016-12-05 09:39:54 +0100

    converted services from xml to yaml

--- DependencyInjection/ZikulaProfileExtension.php
+++ DependencyInjection/ZikulaProfileExtension.php
@@ -12,7 +12,7 @@ namespace Zikula\ProfileModule\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -28,8 +28,8 @@ class ZikulaProfileExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
+        $loader = new YamlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
 
-        $loader->load('services.xml');
+        $loader->load('services.yml');
     }
 }
